7e917aaf010291bbc6ca4adeeac120bff729ea8b
tat131313 <tatyapleshka@gmail.com>
2018-05-23 22:28:02 +0300

    oo

--- index.php
+++ index.php
@@ -1,28 +1,58 @@
 <?php
+      /*$txt = fopen("numbers.txt", "w");           //create random numbers
+        for($i = 0; $i < 100; $i++){
+            $rand = rand(-999, 999);
+            fwrite($txt, "$rand, ");
+        }
+    fclose($txt);*/
+
     $numbers = explode(", ", file_get_contents("numbers.txt"));     //read file and string->array
-    
-    $maxN = $numbers[0];        //calculation max value
-    for($i = 0, $count = count($numbers); $i < $count; $i++){
-        if($numbers[$i] >= $maxN){
-            $maxN = $numbers[$i];
+
+    class numbers{
+        public $maxN;
+        public $minN;
+        public $averageN;
+
+        public function maxN($numbers){             //calculation max value
+            $maxN = $numbers[0];        
+            for($i = 0, $count = count($numbers); $i < $count-1; $i++){
+                if($numbers[$i] >= $maxN){
+                    $maxN = $numbers[$i];
+                }   
+            }
+            return $this->maxN = $maxN;
+        }
+
+        public function minN($numbers){              //calculation min value
+            $minN = $numbers[0];        
+            for($i = 0, $count = count($numbers); $i < $count-1; $i++){
+                if($numbers[$i] <= $minN){
+                    $minN = $numbers[$i];
+                }
+            }
+            return $this->minN = $minN;
+        }   
+
+        public function averageN($numbers){          //calculation average value
+            $averageN = 0;          
+            for($i = 0, $count = count($numbers); $i < $count-1; $i++){
+                $averageN += $numbers[$i];
+            }
+            $averageN = $averageN / count($numbers);
+            return $this->averageN = $averageN;
         }
     }
 
-    $minN = $numbers[0];        //calculation min value
-    for($i = 0, $count = count($numbers); $i < $count; $i++){
-        if($numbers[$i] <= $minN){
-            $minN = $numbers[$i];
-        }
-    }
-
-    $averageN = 0;          //calculation average value
-    for($i = 0, $count = count($numbers); $i < $count; $i++){
-        $averageN += $numbers[$i];
-    }
-    $averageN = $averageN / count($numbers);
-    
     var_dump($numbers);
-    echo "<hr>".'Max number: '.$maxN;
-    echo "<hr>".'Min number: '.$minN;
-    echo "<hr>".'Average value: '.$averageN;
+
+    $time = microtime(true);
+    $number = new numbers;
+    echo "<hr>".'Max number: '.$number->maxN($numbers);
+    echo "<hr>".'Min number: '.$number->minN($numbers);
+    echo "<hr>".'Average value: '.$number->averageN($numbers);
+    echo "<hr>"."Time: $time";
+
+    
+    
+
 ?>
---
+++ index2.php
@@ -0,0 +1,40 @@
+<?php
+    /*$txt = fopen("numbers.txt", "a");               //create random numbers
+        for($i = 0; $i < 1000000; $i++){
+            $rand = rand(-999, 999);
+            fwrite($txt, "$rand, ");
+        }
+    fclose($txt);*/
+
+    
+   $numbers = explode(", ", file_get_contents("numbers.txt"));     //read file and string->array
+    
+    $maxN = $numbers[0];        //calculation max value
+    for($i = 0, $count = count($numbers); $i < $count-1; $i++){
+        if($numbers[$i] >= $maxN){
+            $maxN = $numbers[$i];
+        }
+    }
+
+    $minN = $numbers[0];        //calculation min value
+    for($i = 0, $count = count($numbers); $i < $count-1; $i++){
+        if($numbers[$i] <= $minN){
+            $minN = $numbers[$i];
+        }
+    }
+
+    $averageN = 0;          //calculation average value
+    for($i = 0, $count = count($numbers); $i < $count-1; $i++){
+        $averageN += $numbers[$i];
+    }
+    $averageN = $averageN / count($numbers);
+    
+    var_dump($numbers);
+    echo "<hr>".'Max number: '.$maxN;
+    echo "<hr>".'Min number: '.$minN;
+    echo "<hr>".'Average value: '.$averageN;
+    
+    $time = microtime(true);
+    echo "<hr>"."Time: $time";
+
+?>
